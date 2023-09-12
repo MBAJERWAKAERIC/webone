b1e8241196377536fd71e2112057f0ac0208f408
Mbaje rwaka eric <erickmbanjerwaka@gmail.com>
2023-09-12 19:38:45 +0300

    more samples

---
+++ scrits.js
@@ -0,0 +1,11 @@
+// Using a for loop
+for (var i = 1; i <= 5; i++) {
+    console.log("Iteration " + i);
+}
+
+// Using a while loop
+var count = 0;
+while (count < 3) {
+    console.log("Count: " + count);
+    count++;
+}
